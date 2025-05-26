50e445be33e8e333eef08a0a3de134990f473e36
felixvnolasco84 <felix@polygonag.com>
2025-05-26 14:29:29 -0600

    new logo

--- src/components/ui/Navbar.tsx
+++ src/components/ui/Navbar.tsx
@@ -9,9 +9,7 @@ import MenuMobile from "./MenuMobile";
 import { useLocation } from "react-router-dom";
 import { useEffect } from "react";
 
-import Logo from '@/assets/img/logo/logo.svg'
-
-
+import Logo from '@/assets/img/logo/Nuevo_Logo_Quintazur_Azul.png'
 
 export default function Navbar() {
   const location = useLocation();
@@ -71,7 +69,7 @@ export default function Navbar() {
       <header className="bg-white px-4 py-4 sm:px-6 lg:px-8">
         <div className="mx-auto flex max-w-7xl items-center justify-between">
           <NavLink onClick={() => scrollToSection("inicio")} to="/">
-            <img src={Logo} className="h-16 w-auto" alt="Logo" />
+            <img src={Logo} className="h-20 w-auto" alt="Logo" />
           </NavLink>
           <NavigationMenu>
             <NavigationMenuList className="hidden lg:flex">
